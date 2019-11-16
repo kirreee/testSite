6236134e1b7a9d396b1e148250d26aed5d78a9d1
David Söderberg <david.soderberg97@gmail.com>
2019-11-16 03:31:48 +0100

    Added Advance search settings

--- wwwroot/js/controllers/mainCtrl.js
+++ wwwroot/js/controllers/mainCtrl.js
@@ -11,12 +11,47 @@
 
     //Show link title on mouseover
     $scope.showLinkTxt = function (event) {
-        $(event.currentTarget.firstChild).siblings('.link-title').fadeIn(500);
+        $(event.currentTarget.firstChild).siblings('.link-title').show(200);
     };
 
     //Hide link title on mouseleave
     $scope.hideLinkText = function (event) {
         $(event.currentTarget.firstChild).siblings('.link-title').hide();
     };
+
+
+    //Open advance searchSettings
+    $scope.openAdvanceSearchSettings = function () {
+        $('#advance-search-btn').hide();
+        $('#advanceSearch-content').fadeIn(500);
+    };
+
+
+    $scope.setActiveCarType = function (event) {
+        let targetCarTypeEl = $(event.currentTarget);
+
+        if (targetCarTypeEl.hasClass('inActive-carType')) {
+            targetCarTypeEl.removeClass('inActive-carType').addClass('active-carType');
+            return;
+        }
+
+        targetCarTypeEl.removeClass('active-carType').addClass('inActive-carType');
+    };
+
+
+    //Rounder slide picker year.
+    $("#filter-year-shape").roundSlider({
+        handleShape: "dot",
+        radius: 130,
+        circleShape: "half-top",
+        sliderType: "range",
+        showTooltip: true,
+        change: "traceEventYear",
+        drag: "traceEventYear",
+        max: '2015',
+        value: "1800" + "," + '2015',
+        min: 1800
+    });
+
 
 }]);
